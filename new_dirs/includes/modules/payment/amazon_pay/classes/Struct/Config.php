413e547f51a8e0911625744d1d5286c2fb02ab09
Marcus Kreusch <info@kreusch-creative.de>
2024-07-22 14:05:49 +0200

    version bump

--- new_dirs/includes/modules/payment/amazon_pay/classes/Struct/Config.php
+++ new_dirs/includes/modules/payment/amazon_pay/classes/Struct/Config.php
@@ -3,7 +3,7 @@
 namespace AlkimAmazonPay;
 
 class Config{
-    const PLUGIN_VERSION = '1.3.1';
+    const PLUGIN_VERSION = '1.4.0';
     const PLATFORM_NAME = 'modified2';
     const PLATFORM_ID = 'A1MEF7QTBWK6E';
 }
